46276cc8c2ed2edeb0c42ba076bffd3eade68b74
Sabina <sabina21susuzova@gmail.com>
2026-06-11 15:57:00 +0800

    homework_4

--- product/serializers.py
+++ product/serializers.py
@@ -11,10 +11,29 @@ class CategorySerializer(serializers.ModelSerializer):
     def get_products_count(self, category):
         return category.products.count()
 
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Название категории должно содержать минимум 2 символа")
+        if len (value) > 255:
+            raise serializers.ValidationError("Название категории не не может быть длинее 255 символов")
+        return value
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ["id", "text", "stars", "product"]
+
+        def validate_text(self, value):
+            value = value.strip()
+            if len(value) < 5:
+                raise serializers.ValidationError("Текст отзыва должен содержать минимум 5 символов")
+            return value
+
+        def validate_stars(self, value):
+            if value < 1 or value > 5:
+                raise serializers.ValidationError("Оценка stars должна быть в диапозоне от 1 до 5")
+            return value
+
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -23,12 +42,40 @@ class ProductListSerializer(serializers.ModelSerializer):
         model = Product
         fields = ["id", "title", "price", "category"]
 
+        def validate_title(self, value):
+            value = value.strip()
+            if len(value) < 2:
+                raise serializers.ValidationError("Название товара должно содержать минимум 2 символа")
+            return value
+
+        def validate_price(self, value):
+            if value <= 0:
+                raise serializers.ValidationError("Цена товара должна быть больше 0")
+            return value
+
 class ProductDetailSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     reviews = ReviewSerializer(many=True, read_only=True)
     class Meta:
         model = Product
         fields = "__all__"
+
+    def validate_title(self, value):
+        value = value.strip()
+        if len(value) < 2:
+            raise serializers.ValidationError("Название товара должно содержать минимум 2 символа")
+        return value
+
+    def validate_description(self, value):
+        value = value.strip()
+        if len(value) < 10:
+            raise serializers.ValidationError("Описание товара должно содержать минимум 10 символов")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена товара должна быть больше 0")
+        return value
 
 class ProductReviewsSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
