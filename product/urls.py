6fc6ac2ffd797e6fd53c94dd999bca7b2e75fec2
Sabina <sabina21susuzova@gmail.com>
2026-06-19 17:09:26 +0800

    homework_6

--- product/urls.py
+++ product/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
+from .views import CategoryListCreateView, CategoryDetailView, ProductListCreateView, ProductDetailView, ReviewListCreateView, ReviewDetailView, ProductReviewsListView
 
 urlpatterns = [
-    path("categories/", views.category_list_view),
-    path("categories/<int:id>/", views.category_detail_view),
-    path("products/", views.product_list_view),
-    path("products/<int:id>/", views.product_detail_view),
-    path('products/reviews/', views.product_with_reviews_list_view),
-    path("reviews/", views.review_list_view),
-    path("reviews/<int:id>/", views.review_detail_view),
+    path('categories/', CategoryListCreateView.as_view()),
+    path('categories/<int:pk>/', CategoryDetailView.as_view()),
+    path('products/', ProductListCreateView.as_view()),
+    path('products/<int:pk>/', ProductDetailView.as_view()),
+    path('products/reviews/', ProductReviewsListView.as_view()),
+    path('reviews/', ReviewListCreateView.as_view()),
+    path('reviews/<int:pk>/', ReviewDetailView.as_view()),
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,110 +1,37 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics, status
 from .models import Category, Product, Review
 from .serializers import CategorySerializer, ProductListSerializer, ProductDetailSerializer, ReviewSerializer, ProductReviewsSerializer
 
 
-@api_view(['GET', 'POST'])
-def category_list_view(request):
-    if request.method == 'GET':
-        categories = Category.objects.all()
-        ser = CategorySerializer(categories, many=True)
-        return Response(ser.data)
+class CategoryListCreateView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-    elif request.method == 'POST':
-        ser = CategorySerializer(data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response(ser.data, status=status.HTTP_201_CREATED)
-        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def category_detail_view(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        ser = CategorySerializer(category)
-        return Response(ser.data)
-
-    elif request.method == 'PUT':
-        ser = CategorySerializer(category, data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response(ser.data)
-        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.select_related("category").all()
+    serializer_class = ProductListSerializer
 
 
-@api_view(['GET'])
-def product_list_view(request):
-    products = Product.objects.select_related("category").all()
-    ser = ProductListSerializer(products, many=True)
-    return Response(ser.data)
-
-@api_view(['GET', 'POST'])
-def product_detail_view(request):
-    if request.method == 'GET':
-        product = Product.objects.all()
-        ser = ProductDetailSerializer(product, many=True)
-        return Response(ser.data)
-    elif request.method == 'POST':
-        ser = ProductDetailSerializer(data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response(ser.data, status=status.HTTP_201_CREATED)
-        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
-    try:
-        prod = Product.objects.get(id=id)
-    except Product.DoesNotExist:
-        return Response({"error": "Товар не найден"}, status=status.HTTP_404_NOT_FOUND)
-    ser = ProductDetailSerializer(prod)
-    return Response(ser.data)
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.select_related("category").all()
+    serializer_class = ProductDetailSerializer
 
 
-@api_view(['GET', 'POST'])
-def review_list_view(request):
-    if request.method == 'GET':
-        reviews = Review.objects.all()
-        ser = ReviewSerializer(reviews, many=True)
-        return Response(ser.data)
-
-    elif request.method == 'POST':
-        ser = ReviewSerializer(data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response(ser.data, status=status.HTTP_201_CREATED)
-        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
+class ReviewListCreateView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def review_detail_view(request, pk):
-    try:
-        rev = Review.objects.get(pk=pk)
-    except Review.DoesNotExist:
-        return Response({"error": "Отзыв не найден"}, status=status.HTTP_404_NOT_FOUND)
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
 
-    if request.method == 'GET':
-        ser = ReviewSerializer(rev)
-        return Response(ser.data)
-    elif request.method == 'PUT':
-        ser = ReviewSerializer(rev, data=request.data)
-        if ser.is_valid():
-            ser.save()
-            return Response(ser.data)
-        return Response(ser.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        rev.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET'])
-def product_with_reviews_list_view(request):
-    products = Product.objects.prefetch_related('reviews').all()
-    ser = ProductReviewsSerializer(products, many=True)
-    return Response(ser.data)
+class ProductReviewsListView(generics.ListAPIView):
+    queryset = Product.objects.prefetch_related('reviews').all()
+    serializer_class = ProductReviewsSerializer
